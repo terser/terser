68c6c47bbb77695f2fc17b6975bc3e9c0bd9f76d
alexlamsl <alexlamsl@gmail.com>
2018-02-25 14:44:02 +0800

    [cherry-pick] fix `collapse_vars` on nested exception (#2955) (#2958)
    fixes #2954

--- test/compress/collapse_vars.js
+++ test/compress/collapse_vars.js
@@ -2303,8 +2303,8 @@ unused_orig: {
             var a;
             var c = b;
             for (var d in c) {
-                var a = c[0];
-                return --b + a;
+                var a;
+                return --b + c[0];
             }
             try {
             } catch (e) {
@@ -4964,4 +4964,109 @@ issue_2931: {
         }());
     }
     expect_stdout: "undefined"
+}
+
+issue_2954_1: {
+    options = {
+        collapse_vars: true,
+    }
+    input: {
+        var a = "PASS", b;
+        try {
+            do {
+                b = function() {
+                    throw 0;
+                }();
+                a = "FAIL";
+                b && b.c;
+            } while (0);
+        } catch (e) {
+        }
+        console.log(a);
+    }
+    expect: {
+        var a = "PASS", b;
+        try {
+            do {
+                b = function() {
+                    throw 0;
+                }();
+                a = "FAIL";
+                b && b.c;
+            } while (0);
+        } catch (e) {
+        }
+        console.log(a);
+    }
+    expect_stdout: "PASS"
+}
+
+issue_2954_2: {
+    options = {
+        collapse_vars: true,
+    }
+    input: {
+        var a = "FAIL_1", b;
+        try {
+            throw 0;
+        } catch (e) {
+            do {
+                b = function() {
+                    throw new Error("PASS");
+                }();
+                a = "FAIL_2";
+                b && b.c;
+            } while (0);
+        }
+        console.log(a);
+    }
+    expect: {
+        var a = "FAIL_1", b;
+        try {
+            throw 0;
+        } catch (e) {
+            do {
+                a = "FAIL_2";
+                (b = function() {
+                    throw new Error("PASS");
+                }()) && b.c;
+            } while (0);
+        }
+        console.log(a);
+    }
+    expect_stdout: Error("PASS")
+}
+
+issue_2954_3: {
+    options = {
+        collapse_vars: true,
+    }
+    input: {
+        var a = "FAIL_1", b;
+        try {
+        } finally {
+            do {
+                b = function() {
+                    throw new Error("PASS");
+                }();
+                a = "FAIL_2";
+                b && b.c;
+            } while (0);
+        }
+        console.log(a);
+    }
+    expect: {
+        var a = "FAIL_1", b;
+        try {
+        } finally {
+            do {
+                a = "FAIL_2";
+                (b = function() {
+                    throw new Error("PASS");
+                }()) && b.c;
+            } while (0);
+        }
+        console.log(a);
+    }
+    expect_stdout: Error("PASS")
 }

052f37dc366d1631824ce4367fef435cc702637b
Fábio Santos <fabiosantosart@gmail.com>
2018-06-15 19:09:26 +0100

    Revert "[port] 4c5b9d3 general clean-ups (part 1)"
    This reverts commit f5b5aea0c44c2a507b86d5d079f9f098371874a4.

--- test/mocha/number-literal.js
+++ test/mocha/number-literal.js
@@ -7,15 +7,16 @@ describe("Number literals", function () {
             '"use strict";00;',
             '"use strict"; var foo = 00;'
         ];
+
         var test = function(input) {
             return function() {
                 uglify.parse(input);
             }
-        };
+        }
         var error = function(e) {
-            return e instanceof uglify.JS_Parse_Error
-                && e.message === "Legacy octal literals are not allowed in strict mode";
-        };
+            return e instanceof uglify.JS_Parse_Error &&
+                e.message === "Legacy octal literals are not allowed in strict mode";
+        }
         for (var i = 0; i < inputs.length; i++) {
             assert.throws(test(inputs[i]), error, inputs[i]);
         }

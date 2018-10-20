031ffafe2e48c58b83eb7fdf2a886ee1baa53111
Fábio Santos <fabiosantosart@gmail.com>
2018-10-20 19:09:22 +0100

    apply @kzc's patch and close #130

--- tools/exports.js
+++ tools/exports.js
@@ -2,5 +2,6 @@ exports["Dictionary"] = Dictionary;
 exports["minify"] = minify;
 exports["parse"] = parse;
 exports["push_uniq"] = push_uniq;
+exports["OutputStream"] = OutputStream;
 exports["TreeTransformer"] = TreeTransformer;
 exports["TreeWalker"] = TreeWalker;
--- tools/node.js
+++ tools/node.js
@@ -27,47 +27,11 @@ new Function("MOZ_SourceMap", "exports", function() {
         if (global.__IS_TESTING__) return instrumenter.instrumentSync(contents, file);
         return contents;
     });
-    code.push("exports.describe_ast = " + describe_ast.toString());
     return code.join("\n\n");
 }())(
     require("source-map"),
     UglifyJS
 );
-
-function describe_ast() {
-    var out = OutputStream({ beautify: true });
-    function doitem(ctor) {
-        out.print("AST_" + ctor.TYPE);
-        var props = ctor.SELF_PROPS.filter(function(prop){
-            return !/^\$/.test(prop);
-        });
-        if (props.length > 0) {
-            out.space();
-            out.with_parens(function(){
-                props.forEach(function(prop, i){
-                    if (i) out.space();
-                    out.print(prop);
-                });
-            });
-        }
-        if (ctor.documentation) {
-            out.space();
-            out.print_string(ctor.documentation);
-        }
-        if (ctor.SUBCLASSES.length > 0) {
-            out.space();
-            out.with_block(function(){
-                ctor.SUBCLASSES.forEach(function(ctor, i){
-                    out.indent();
-                    doitem(ctor);
-                    out.newline();
-                });
-            });
-        }
-    };
-    doitem(AST_Node);
-    return out + "\n";
-}
 
 function infer_options(options) {
     var result = UglifyJS.minify("", options);

507070a235ec9d0a7b06be4722d654013ba2870c
Fábio Santos <fabiosantosart@gmail.com>
2018-10-25 20:27:31 +0100

    have `drop_console` ignore argument side effects as documented

--- test/compress/drop-console.js
+++ test/compress/drop-console.js
@@ -24,3 +24,25 @@ drop_console_2: {
         void 0;
     }
 }
+
+unexpected_side_effects_dropping_console: {
+    options = {
+        drop_console: true,
+        evaluate: true,
+        reduce_vars: true,
+        side_effects: true,
+        unused: true,
+    }
+    input: {
+        function f() {
+            var a = 33;
+            console.log(a++);
+            alert(a);
+        }
+    }
+    expect: {
+        function f() {
+            alert(33);
+        }
+    }
+}

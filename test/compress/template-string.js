aac47c4356f19444a3f7a8069d1fc864a1ec4df6
Fábio Santos <fabiosantosart@gmail.com>
2018-10-26 22:49:14 +0100

    apply @kzc's patch

--- test/compress/template-string.js
+++ test/compress/template-string.js
@@ -1,3 +1,19 @@
+tagged_template_parens: {
+    input: {
+        (a) `0`;
+        (a => b) `1`;
+        (a = b) `2`;
+        (a + b) `3`;
+        (a ? b : c) `4`;
+        (a, b, c) `5`;
+        (~a) `6`;
+        (a.b) `7`;
+        (a["b"]) `8`;
+        (a()) `9`;
+    }
+    expect_exact: 'a`0`;(a=>b)`1`;(a=b)`2`;(a+b)`3`;(a?b:c)`4`;(a,b,c)`5`;(~a)`6`;a.b`7`;a["b"]`8`;a()`9`;'
+}
+
 template_strings: {
     beautify = {
         quote_style: 3

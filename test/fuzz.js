0dca44aae2317f8d3137ec21c32098859da0c7d7
Björgvin Ragnarsson <nifgraup@gmail.com>
2018-12-16 21:20:58 +0200

    A new ES6 capable fuzzer based on eslump
    11 issue have been identified where acorn is able parse input but terser
    fails and 10 issues for vice versa. These issue have been ignored so the
    fuzzer will run in an endless loop.

---
+++ test/fuzz.js
@@ -0,0 +1,59 @@
+#! /usr/bin/env node
+
+var acorn = require("acorn");
+var generateRandomJS = require("eslump").generateRandomJS;
+var minify = require("..").minify;
+
+var known_terser_errors = new RegExp([
+  "Cannot negate a statement",
+  "Cannot read property 'references' of undefined",
+  "Cannot read property 'type' of null",
+  "Cannot read property 'value' of undefined",
+  "Octal escape sequences are not allowed in template strings",
+  "Parameter .* was used already",
+  "redeclared",
+  "t.argname.definition is not a function",
+  "Unexpected token: operator", // issue 199
+  "Unexpected token: template_substitution",
+  "Unexpected yield identifier inside strict mode",
+].join("|"));
+
+var known_acorn_errors = new RegExp([
+  "Argument name clash",
+  "Comma is not permitted after the rest element",
+  "Duplicate export",
+  "Expecting Unicode escape sequence",
+  "Identifier '.*' has already been declared",
+  "Invalid regular expression: .* Unmatched",
+  "Invalid regular expression: .* Unterminated group",
+  "Octal literal in strict mode",
+  "Unexpected token",
+  "Unterminated regular expression",
+].join("|"));
+
+while (true) {
+  var input = generateRandomJS();
+
+  try {
+    acorn.parse(input, {
+      sourceType: "module",
+    });
+  } catch (e) {
+    if (!known_acorn_errors.test(e.message)) {
+      var result = minify(input);
+
+      if (!result.error) {
+        console.log(input);
+        throw e;
+      }
+    }
+    continue;
+  }
+
+  var result = minify(input);
+
+  if (result.error && !known_terser_errors.test(result.error.message)) {
+    console.log(input);
+    throw result.error;
+  }
+}

660266873f4d9031636092c9efd1da011f49ba03
silverwind <me@silverwind.io>
2018-12-19 19:23:41 +0100

    Make tests compatible with Node.js 12 and prerelease versions (#197)
    * make version check work with prerelease versions
    
    semver.satisfies does not match a prerelease version against a
    non-prerelease one, e.g. 12.0.0-pre does not match >= 10. Use a regex
    match to workaround this fact.
    
    * make tests compatible with Node.js 12
    
    In Node.js 12, the formatting of console arguments will change slightly.
    Previously, a string other than the first argument was formatted using
    single quotes if the first argument was non-string. Now, quotes are
    never added regardless of position of a string argument.
    
    To make test compatible in all Node.js versions, I work around by
    ensuring the first argument to console.log is a string which prevents
    the quotes from being added on older versions of Node.js.
    
    Ref: https://github.com/nodejs/node/pull/23162
    
    * restart travis

--- test/compress/string-literal.js
+++ test/compress/string-literal.js
@@ -15,14 +15,14 @@ issue_1929: {
             return s.split(/[\\/]/);
         }
         var r = f("A/B\\C\\D/E\\F");
-        console.log(r.length, r[5], r[4], r[3], r[2], r[1], r[0]);
+        console.log(r[5], r[4], r[3], r[2], r[1], r[0], r.length);
     }
     expect: {
         function f(s) {
             return s.split(/[\\/]/);
         }
         var r = f("A/B\\C\\D/E\\F");
-        console.log(r.length, r[5], r[4], r[3], r[2], r[1], r[0]);
+        console.log(r[5], r[4], r[3], r[2], r[1], r[0], r.length);
     }
-    expect_stdout: "6 'F' 'E' 'D' 'C' 'B' 'A'"
+    expect_stdout: "F E D C B A 6"
 }

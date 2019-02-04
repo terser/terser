85b4ae85f07f307bf6bbd134ba868cb95561c74c
Andrey Chalkin <andrey.chalkin@sidenis.com>
2019-02-04 19:20:28 +0700

    Replace only if unsafe option is turned on

--- test/compress/array-constructor.js
+++ test/compress/array-constructor.js
@@ -7,10 +7,10 @@ array_constructor: {
         console.log(new Array(12));
     }
     expect: {
-        console.log([]);
-        console.log([]);
-        console.log([,]);
-        console.log([,,,,,,,,,,,]);
+        console.log(new Array());
+        console.log(new Array(0));
+        console.log(new Array(1));
+        console.log(new Array(11));
         console.log(new Array(12));
     }
 }

8b2111a8a09e9fb4c8c22a26d710cac985a20f48
Mattias Buelens <mattias@buelens.com>
2019-03-01 01:04:25 +0100

    Remove Dictionary class

--- tools/terser.d.ts
+++ tools/terser.d.ts
@@ -179,21 +179,6 @@ export class TreeTransformer extends TreeWalker {
 
 export function push_uniq<T>(array: T[], el: T): void;
 
-type DictEachCallback = (val: any, key: string) => any;
-
-export class Dictionary {
-    static fromObject(obj: object): Dictionary;
-    add(key: string, val: any): this;
-    clone(): Dictionary;
-    del(key: string): this;
-    each(fn: DictEachCallback): void;
-    get(key: string): any;
-    has(key: string): boolean;
-    map(fn: DictEachCallback): any[];
-    set(key: string, val: any): this;
-    size(): number;
-}
-
 export function minify(files: string | string[] | { [file: string]: string } | AST_Node, options?: MinifyOptions): MinifyOutput;
 
 export class AST_Node {

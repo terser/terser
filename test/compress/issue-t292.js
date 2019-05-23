936a7cba210301a4e870694d5d088447285f91b4
j-verse <44666223+j-verse@users.noreply.github.com>
2019-05-23 19:03:17 +0200

    Compression function inlining/flattening argument/var names colliding with argument values inner scopes (#307)
    * Compress lib changed to account for overriding of names in inner scopes of arguments when flattening
    
    * Can inject arg values now follows same rules as can inject args when testing for collisions, removed safe_to_inject as this should already have been checked by can inject args/vals
    
    * Added tests to verify flattening no longer happens for arg values to arg name or var collisions
    
    * Replaced object maps with Sets, renamed some variables to make more sense and checked for AST_Destructuring when checking function var names to make sure name.name is available
    
    * Added check to skip checking args and vars when none of the arg values refer to outer scopes

---
+++ test/compress/issue-t292.js
@@ -0,0 +1,78 @@
+no_flatten_with_arg_colliding_with_arg_value_inner_scope: {
+    options = {
+        collapse_vars: true,
+        inline: true,
+        reduce_funcs: true,
+        reduce_vars: true,
+        sequences: true,
+        toplevel: true,
+        unused: true,
+    }
+    input: {
+        var g = ["a"];
+        function problem(arg) {
+            return g.indexOf(arg);
+        }
+        function unused(arg) {
+            return problem(arg);
+        }
+        function a(arg) {
+            return problem(arg);
+        }
+        function b(problem) {
+            return g[problem];
+        }
+        function c(arg) {
+            return b(a(arg));
+        }
+        console.log(c("a"));
+    }
+    expect: {
+        var g=["a"];
+        function problem(arg){return g.indexOf(arg)}
+        console.log(function(problem){return g[problem]}(function(arg){return problem(arg)}("a")));
+    }
+    expect_stdout: "a"
+}
+
+no_flatten_with_var_colliding_with_arg_value_inner_scope: {
+    options = {
+        collapse_vars: true,
+        inline: true,
+        reduce_funcs: true,
+        reduce_vars: true,
+        sequences: true,
+        toplevel: true,
+        unused: true,
+    }
+    input: {
+        var g = ["a"];
+        function problem(arg) {
+            return g.indexOf(arg);
+        }
+        function unused(arg) {
+            return problem(arg);
+        }
+        function a(arg) {
+            return problem(arg);
+        }
+        function b(test) {
+            var problem = test * 2;
+            console.log(problem);
+            return g[problem];
+        }
+        function c(arg) {
+            return b(a(arg));
+        }
+        console.log(c("a"));
+    }
+    expect: {
+        var g=["a"];
+        function problem(arg){return g.indexOf(arg)}
+        console.log(function(test){var problem=2*test;return console.log(problem),g[problem]}(function(arg){return problem(arg)}("a")));
+    }
+    expect_stdout: [
+        "0",
+        "a",
+    ]
+}

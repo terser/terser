5792726bf8d070dac9fe1712260cedb05fd2b7e3
Fábio Santos <fabiosantosart@gmail.com>
2019-12-15 23:30:57 +0000

    fix functional tests and speed up older tests

--- test/mocha/release.js
+++ test/mocha/release.js
@@ -2,8 +2,6 @@ var assert = require("assert");
 var semver = require("semver");
 var spawn = require("child_process").spawn;
 
-if (!process.env.TERSER_TEST_ALL) return;
-
 function run(command, args, done) {
     spawn(command, args, {
         stdio: [ "ignore", 1, 2 ]
@@ -13,19 +11,17 @@ function run(command, args, done) {
     });
 }
 
-if (semver.satisfies(process.version, "6")) return;
+if (!process.env.TERSER_TEST_ALL
+    || !semver.satisfies(process.version, "12")
+) {
+    return;
+}
 
 describe("test/benchmark.js", function() {
     this.timeout(10 * 60 * 1000);
     [
-        "-b",
-        "-b braces",
-        "-m",
-        "-mc passes=3",
-        "-mc passes=3,toplevel",
-        "-mc passes=3,unsafe",
-        "-mc keep_fargs=false,passes=3",
-        "-mc keep_fargs=false,passes=3,pure_getters,unsafe,unsafe_comps,unsafe_math,unsafe_proto",
+        "-mc toplevel",
+        "-mc keep_fargs=false,pure_getters,unsafe,unsafe_comps,unsafe_math,unsafe_proto",
     ].forEach(function(options) {
         it("Should pass with options " + options, function(done) {
             var args = options.split(/ /);
@@ -39,7 +35,7 @@ describe("test/jetstream.js", function() {
     this.timeout(20 * 60 * 1000);
     [
         "-mc",
-        "-mc keep_fargs=false,passes=3,pure_getters,unsafe,unsafe_comps,unsafe_math,unsafe_proto",
+        "-mc keep_fargs=false,pure_getters,unsafe,unsafe_comps,unsafe_math,unsafe_proto",
     ].forEach(function(options) {
         it("Should pass with options " + options, function(done) {
             var args = options.split(/ /);

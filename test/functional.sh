00ea641c80f16094e1e1d09074741a4722da1fc1
Fábio Santos <fabiosantosart@gmail.com>
2020-01-05 19:12:02 +0000

    document and improve functional tests

--- test/functional.sh
+++ test/functional.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-set -exo pipefail
+set -exuo pipefail
 
-npm run build
+# for tests that need terser-under-test path and @terser/require-terser
+TERSER_PATH="$(pwd)"
+
+# build terser
+npm run build -- --configTest
+
+# grab the functional tests
 mkdir -p terser-functional-tests
 cd terser-functional-tests
 git checkout . || true
 git clone https://github.com/terser/terser-functional-tests --depth 1 . || true
 git pull
+
+# install packages and link terser in (can't npm link .., it crashes)
 npm ci
-npm link ..
-TERSER_PATH="$(pwd)/.." ./run-all.sh
+(cd node_modules && rm -rf terser && ln -s ../.. terser)
+
+# source the script so it can use our environment (for nvm, which is a shell function, to work)
+TERSER_PATH="$TERSER_PATH" ./run-all.sh

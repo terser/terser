7a48cc0696db61015f77b9f2dd34e4c232abb980
Kruithne <kruithne@gmail.com>
2020-03-03 23:03:48 +0000

    add tests for unsafe_symbols

---
+++ test/compress/unsafe_symbols.js
@@ -0,0 +1,17 @@
+unsafe_symbols_1: {
+    options = {}
+    input: {
+        Symbol("kDog");
+    }
+    expect_exact: 'Symbol("kDog");'
+}
+
+unsafe_symbols_2: {
+    options = {
+        unsafe_symbols: true
+    }
+    input: {
+        Symbol("kDog");
+    }
+    expect_exact: 'Symbol();'
+}

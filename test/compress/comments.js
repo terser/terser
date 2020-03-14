d6d010c00abf3bfbdcc928fd1f08952e6e5e6415
Fábio Santos <fabiosantosart@gmail.com>
2020-03-14 01:06:30 +0000

    avoid comments being moved between return and its value. closes #597

--- test/compress/comments.js
+++ test/compress/comments.js
@@ -38,3 +38,25 @@ preserve_comments_by_default: {
         "/*! foo */",
     ]
 }
+
+comment_moved_between_return_and_value: {
+    beautify = {
+        comments: "some"
+    }
+    input: {
+        console.log(function (same_name) {
+            /* @license Foo bar */
+            function licensed(same_name) {
+                return same_name.toUpperCase()
+            }
+
+            console.log("PASS")
+
+            return licensed("PA") + "SS"
+        }())
+    }
+    expect_stdout: [
+        "PASS",
+        "PASS"
+    ]
+}

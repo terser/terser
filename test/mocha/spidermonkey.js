dda02924e5a85f5709142c62aff3054c61f15543
Justin Ridgewell <justin@ridgewell.name>
2020-04-05 21:57:48 -0400

    Fix default destucture assignments from Mozilla AST

--- test/mocha/spidermonkey.js
+++ test/mocha/spidermonkey.js
@@ -117,6 +117,17 @@ describe("spidermonkey export/import sanity test", function() {
         );
     });
 
+    it("should correctly minify AST from from_moz_ast with default destructure", () => {
+        const code = "const { a = 1, b: [b = 2] = []} = {}";
+        const acornAst = acorn.parse(code, { locations: true });
+        const terserAst = Terser.AST_Node.from_mozilla_ast(acornAst);
+        const result = Terser.minify(terserAst, {ecma: 2015});
+        assert.strictEqual(
+            result.code,
+            "const{a=1,b:[b=2]=[]}={};"
+        );
+    });
+
     it("should correctly minify AST from from_moz_ast with default function parameter", () => {
         const code = "function run(x = 2){}";
         const acornAst = acorn.parse(code, { locations: true });

743d032f5222cd15c19e0c71f6ade0a519fd584a
Fábio Santos <fabiosantosart@gmail.com>
2020-04-23 13:36:13 +0100

    add a marker between nodes while recursively comparing them in equivalent_to.
    Closes #662

--- test/mocha/equivalent-to.js
+++ test/mocha/equivalent-to.js
@@ -1,13 +1,18 @@
-'use strict'
+"use strict";
 
-const assert = require('assert')
-const terser = require('../../')
+const assert = require("assert");
+const terser = require("../../");
 
-describe('equivalent_to', () => {
-    it('(regression) two regexes should not be equivalent if their source or flags differ', () => {
-        const ast = terser.parse('/^\s*$/u')
-        const ast2 = terser.parse('/^\s*\*/u')
-        assert.equal(ast.equivalent_to(ast2), false)
-    })
-})
+describe("equivalent_to", () => {
+    it("(regression) two regexes should not be equivalent if their source or flags differ", () => {
+        const ast = terser.parse("/^\s*$/u");
+        const ast2 = terser.parse("/^\s*\*/u");
+        assert.equal(ast.equivalent_to(ast2), false);
+    });
+    it("nested calls should not be equivalent even if a tree walk reveals equivalent nodes", () => {
+        const ast = terser.parse("hello(1, world(2), 3)");
+        const ast2 = terser.parse("hello(1, world(2, 3))");
+        assert.equal(ast.equivalent_to(ast2), false);
+    });
+});
 

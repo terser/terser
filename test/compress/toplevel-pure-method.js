4ade54a08e1b3855195cf2155c2b2b3810bf67f9
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-01-12 11:51:21 -0600

    test: inline_different_name

--- test/compress/toplevel-pure-method.js
+++ test/compress/toplevel-pure-method.js
@@ -20,3 +20,28 @@ assign_and_inline: {
         new class{method(a1) {var a;a=a1,console.log(a)}};
     }
 }
+
+inline_different_name: {
+    options = {
+        toplevel: true,
+        defaults: true,
+        pure_getters: true
+    }
+    input: {
+        class MyClass {
+          method(a1) {
+           myPureFunction(a1)
+          }
+        }
+
+        function myPureFunction (a) {
+         console.log(a)
+        }
+
+        new MyClass();
+    }
+    expect: {
+        new class{method(a1) {console.log(a1)}};
+    }
+}
+

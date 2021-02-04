3d6faefc73f5bcda5d03b05a9552ae13f76869f4
Robin Ricard <rricard2@bloomberg.net>
2021-02-04 22:41:38 +0000

    test private fields as accessors (#923)

--- test/compress/classes.js
+++ test/compress/classes.js
@@ -135,3 +135,53 @@ private_class_methods: {
     }
     // expect_stdout: "PASS" // < tested in chrome, fails with nodejs 14 (current LTS)
 }
+
+private_class_accessors: {
+    no_mozilla_ast = true;
+    node_version = ">=12"
+    input: {
+        class A {
+            #accessorInternal = "FAIL"
+            get #accessor() {
+                return this.#accessorInternal
+            }
+            set #accessor(v) {
+                this.#accessorInternal = v;
+            }
+            static get #accessor2() {
+                return "S"
+            }
+            get ["#accessor"]() {
+                return "S"
+            }
+            async print() {
+                this.#accessor = "PA"
+                console.log(this.#accessor + A.#accessor2 + this["#accessor"]);
+            }
+        }
+        new A().print();
+    }
+    expect: {
+        class A {
+            #accessorInternal = "FAIL"
+            get #accessor() {
+                return this.#accessorInternal
+            }
+            set #accessor(v) {
+                this.#accessorInternal = v;
+            }
+            static get #accessor2() {
+                return "S"
+            }
+            get ["#accessor"]() {
+                return "S"
+            }
+            async print() {
+                this.#accessor = "PA"
+                console.log(this.#accessor + A.#accessor2 + this["#accessor"]);
+            }
+        }
+        new A().print();
+    }
+    // expect_stdout: "PASS" // < tested in chrome, fails with nodejs 14 (current LTS)
+}

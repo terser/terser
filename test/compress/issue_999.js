60a74bb1679107e03a0f498b45644991dec2b19a
Kubilay Kahveci <kahvecikubilay@gmail.com>
2021-06-23 19:28:41 +0100

    fix: private class element access equality (#1015)

---
+++ test/compress/issue_999.js
@@ -0,0 +1,41 @@
+switch_case_and_private_fields: {
+    no_mozilla_ast = true;
+    node_version = ">=12"
+    options = {
+        switches: true,
+        dead_code: true,
+    }
+    input: {
+        class A {
+            #a = "FAIL";
+            #b = "PASS";
+
+            print(variant) {
+                switch (variant) {
+                    case 1:
+                        return this.#a;
+                    case 2:
+                        return this.#b;
+                }
+            }
+        }
+        console.log(new A().print(2));
+    }
+    expect: {
+        class A {
+            #a = "FAIL";
+            #b = "PASS";
+
+            print(variant) {
+                switch (variant) {
+                    case 1:
+                        return this.#a;
+                    case 2:
+                        return this.#b;
+                }
+            }
+        }
+        console.log(new A().print(2));
+    }
+    expect_stdout: "PASS"
+}

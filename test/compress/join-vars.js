fa6f16467165730aff45f6165afce33714c32dc8
Fábio Santos <fabiosantosart@gmail.com>
2023-02-23 10:16:41 +0000

    don't assume transform functions don't mutate AST arrays. Closes #1351

--- test/compress/join-vars.js
+++ test/compress/join-vars.js
@@ -38,3 +38,44 @@ issue_1079_with_mixed: {
         for (let i = 0; i < netmaskBits; ++i) netmaskBinary += '1';
     }
 }
+
+join_vars_lose_other_var: {
+    options = {
+        defaults: false,
+        inline: true,
+        reduce_vars: true,
+        unused: true,
+        side_effects: true,
+        join_vars: true,
+    }
+    input: {
+        global.exports = {
+            set a(a) {
+                console.log(a().get_b())
+                const incremented = a()
+                incremented.inc_b()
+                console.log(incremented.get_b())
+            }
+        };
+
+        (function (factory) {
+            factory(exports)
+        })((function (exports) {
+            exports.a = function () {
+                var b = "PASS",
+                    _this = {};
+                _this.get_b = function () {
+                    return b;
+                }
+                _this.inc_b = function () {
+                    b += "PASS";
+                }
+                return _this;
+            };
+        }));
+    }
+    expect_stdout: [
+      "PASS",
+      "PASSPASS"
+    ]
+}

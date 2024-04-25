72763952c537f63f46f89434246f379a5055bd83
mlgq <ggqliang@gmail.com>
2024-04-26 00:28:48 +0800

    fixed an error using import syntax when the  field is  in tsconfig.json

---
+++ bin/publish.mjs
@@ -0,0 +1,26 @@
+import fs from "fs"
+import path from "path"
+import { fileURLToPath } from "url"
+
+const publishHookType = process.argv[2]
+
+const __dirname = fileURLToPath(import.meta.url)
+const packageJsonPath = path.resolve(__dirname, "../../package.json")
+const backupPath = path.resolve(__dirname, "../../.backup-package.json")
+
+const readFile = (path) => fs.readFileSync(path, "utf-8")
+const writeFile = (path, content) => fs.writeFileSync(path, content, "utf-8")
+
+if (publishHookType === "pre" && !fs.existsSync(backupPath)) {
+    const packageJson = readFile(packageJsonPath)
+    const packageObj = JSON.parse(packageJson)
+    writeFile(backupPath, packageJson)
+    delete packageObj.type
+
+    const newPackageJson = JSON.stringify(packageObj, null, 2)
+    writeFile(packageJsonPath, newPackageJson)
+} else if (publishHookType === "post") {
+    const packageJson = readFile(backupPath)
+    writeFile(packageJsonPath, packageJson)
+    fs.unlinkSync(backupPath)
+}

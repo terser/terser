aa359375d88d088d4421c99e1e6831e9285b0ee3
Fábio Santos <fabiosantosart@gmail.com>
2025-06-09 14:44:13 +0100

    estree: overhaul conversion code and test feature parity with Acorn. Closes #365

--- test/mocha/spidermonkey.js
+++ test/mocha/spidermonkey.js
@@ -1,3 +1,4 @@
+import { inspect } from "util";
 import assert from "assert";
 import fs from "fs";
 import * as acorn from "acorn";
@@ -100,7 +101,7 @@ describe("spidermonkey export/import sanity test", function() {
         }
     });
 
-    it("should output and parse ES6 code correctly", async function() {
+    it("should output and parse ESTree correctly", async function() {
         var code = fs.readFileSync("test/input/spidermonkey/input.js", "utf-8");
         var terser_ast = parse(code);
         var moz_ast = terser_ast.to_mozilla_ast();
@@ -109,21 +110,59 @@ describe("spidermonkey export/import sanity test", function() {
             from_moz_ast.print_to_string(),
             terser_ast.print_to_string()
         );
+        assert(
+            compare_asts(from_moz_ast, terser_ast),
+            "after ESTree output>input, the output is the same but the AST is different"
+        );
     });
 
-    it("should be capable of importing from acorn", async function() {
-        var code = fs.readFileSync("test/input/spidermonkey/input.js", "utf-8");
+    it("(temp): should output and parse ESTree correctly (astring incompatible bits)", async function() {
+        var code = fs.readFileSync("test/input/spidermonkey/input-no-astring.js", "utf-8");
         var terser_ast = parse(code);
-        var moz_ast = acornParse(code, { sourceType: "module", ecmaVersion: 2023 });
+        var moz_ast = terser_ast.to_mozilla_ast();
         var from_moz_ast = AST.AST_Node.from_mozilla_ast(moz_ast);
         assert.strictEqual(
             from_moz_ast.print_to_string(),
             terser_ast.print_to_string()
         );
+        assert(
+            compare_asts(from_moz_ast, terser_ast),
+            "after ESTree output>input, the output is the same but the AST is different"
+        );
+    })
+
+    it("should be capable of importing from acorn", async function() {
+        var code = fs.readFileSync("test/input/spidermonkey/input.js", "utf-8");
+        var terser_ast = parse(code);
+        var moz_ast = acornParse(code, { sourceType: "module", ecmaVersion: 2025 });
+        var from_moz_ast = AST.AST_Node.from_mozilla_ast(moz_ast);
+        assert.strictEqual(
+            from_moz_ast.print_to_string(),
+            terser_ast.print_to_string()
+        );
+        assert(
+            compare_asts(from_moz_ast, terser_ast),
+            "parsing ESTree from Acorn and Terser, the output is the same but the AST is different"
+        );
     });
 
+    it("(temp): should be capable of importing from acorn (astring incompatible bits)", async function() {
+        var code = fs.readFileSync("test/input/spidermonkey/input-no-astring.js", "utf-8");
+        var terser_ast = parse(code);
+        var moz_ast = acornParse(code, { sourceType: "module", ecmaVersion: 2025 });
+        var from_moz_ast = AST.AST_Node.from_mozilla_ast(moz_ast);
+        assert.strictEqual(
+            from_moz_ast.print_to_string(),
+            terser_ast.print_to_string()
+        );
+        assert(
+            compare_asts(from_moz_ast, terser_ast),
+            "parsing ESTree from Acorn and Terser, the output is the same but the AST is different"
+        );
+    })
+
     it("should accept a spidermonkey AST w/ `parse.spidermonkey: true`", async function() {
-        var moz_ast = acornParse("var a = 1 + 2", { sourceType: "module", ecmaVersion: 2023 });
+        var moz_ast = acornParse("var a = 1 + 2", { sourceType: "module", ecmaVersion: 2025 });
         var result = await minify(moz_ast, {ecma: 2015, parse: {spidermonkey: true}});
         assert.deepStrictEqual(result.code, "var a=3;");
     });
@@ -160,26 +199,270 @@ describe("spidermonkey export/import sanity test", function() {
         );
     });
 
-    it("should correctly minify AST from from_moz_ast with default function parameter", async () => {
-        const code = "function run(x = 2){}";
-        const acornAst = acornParse(code, { locations: true, ecmaVersion: 2023 });
-        const terserAst = AST.AST_Node.from_mozilla_ast(acornAst);
-        const result = await minify(terserAst);
-        assert.strictEqual(
-            result.code,
-            "function run(x=2){}"
-        );
-    });
-
     it("should produce an AST compatible with astring", async function() {
         var code = fs.readFileSync("test/input/spidermonkey/input.js", "utf-8");
         var terser_ast = parse(code);
         var moz_ast = terser_ast.to_mozilla_ast();
         var generated = astringGenerate(moz_ast);
-        var parsed = acornParse(generated, { sourceType: "module", ecmaVersion: 2023 });
+        var parsed = acornParse(generated, { sourceType: "module", ecmaVersion: 2025 });
         assert.strictEqual(
             AST.AST_Node.from_mozilla_ast(parsed).print_to_string(),
             terser_ast.print_to_string()
         );
+        assert(
+            compare_asts(AST.AST_Node.from_mozilla_ast(parsed), terser_ast),
+            "after astring, the output is the same but the AST is different"
+        );
+    });
+
+    it("should generate the same AST that acorn does", async function () {
+        var code = fs.readFileSync("test/input/spidermonkey/input.js", "utf-8");
+        var terser_ast = parse(code).to_mozilla_ast();
+        var acorn_ast = acornParse(code, { sourceType: "module", ecmaVersion: 2025 });
+        compare_estree_asts(terser_ast, acorn_ast)
     });
 });
+
+function compare_estree_asts(actual, expected) {
+    const skip_keys = new Set(['loc', 'start', 'end', 'sourceType'])
+    const skip_node_keys = {
+        Program: new Set(['sourceType']),
+        FunctionExpression: new Set(['expression']),
+        FunctionDeclaration: new Set(['expression']),
+        ArrowFunctionExpression: new Set(['expression', 'generator']),
+    }
+
+    function pojo_clone(node) {
+        const out = { type: node.type }
+
+        for (const [key, child] of Object.entries(node)) {
+            // ESTree node
+            if (typeof child?.type === 'string') {
+                out[key] = pojo_clone(child)
+            } else if (Array.isArray(child)) {
+                out[key] = child.map(
+                    c => c && typeof c === 'object' ? pojo_clone(c) : c
+                )
+            } else {
+                out[key] = child
+            }
+        }
+
+        return out
+    }
+
+    actual = pojo_clone(actual)
+    expected = pojo_clone(expected)
+
+    try {
+        deep_cmp(actual, expected);
+    } catch (err) {
+        if (err instanceof assert.AssertionError && err.node1) {
+            if (err.node1.loc) {
+                err.message += `\non line ${err.node1.loc.start.line}`;
+            }
+            if (err.key) {
+                err.message += `\nKey "${err.key}" in ${err.node1.type} node`;
+            } else {
+                err.message += `\nIn ${err.node1.type} node`;
+            }
+        }
+        if (err instanceof assert.AssertionError) {
+            throw new Error(err.message);
+        }
+        throw err;
+    }
+
+    function deep_cmp(actual, expected) {
+        const obj_keys = new Set([
+            ...Object.keys(actual),
+            ...Object.keys(expected),
+        ]);
+        for (const key of obj_keys) {
+            if (skip_keys.has(key)) {
+                continue;
+            }
+            if (skip_node_keys?.[actual.type]?.has(key)) {
+                continue;
+            }
+            if (typeof expected[key] === "bigint") {
+                continue; // Terser won't emit bigint values
+            }
+            if (key === "value" && actual.type == "Literal" && actual.regex) {
+                continue; // "value" is emitted as a string from Acorn, but null from Terser.
+            }
+
+            const node1 = actual[key];
+            const node2 = expected[key];
+
+            // don't bother with optional keys
+            {
+                if (node1 == null && node2 == null) continue;
+
+                let not_nil;
+                if (node1 == null) not_nil = node2;
+                if (node2 == null) not_nil = node1;
+
+                if (not_nil && Array.isArray(not_nil)) continue;
+            }
+
+            if (
+                node1
+                && node2
+                && typeof node1?.type === 'string'
+                && typeof node2?.type === 'string'
+            ) {
+                try {
+                    deep_cmp(node1, node2);
+                } catch(err) {
+                    if (!err.node1) {
+                        err.node1 = node1;
+                        err.node2 = node2;
+                    }
+                    throw err;
+                }
+            } else if (
+                node1
+                && node2
+                && typeof node1 === 'object'
+                && typeof node2 === 'object'
+            ) {
+                deep_cmp(node1, node2)
+            } else {
+                shallow_cmp(key, node1, node2);
+            }
+        }
+    }
+
+    function shallow_cmp(key, actual, expected) {
+        try {
+            assert.deepStrictEqual(actual, expected);
+        } catch (e) {
+            e.key = key;
+            throw e;
+        }
+    }
+}
+
+function compare_asts(tree1, tree2) {
+    const ignore_keys = new Set(['start', 'end']);
+
+    if (!shallow_cmp(tree1, tree2)) return false;
+
+    // Simultaneous preorder traversal
+    const walk_1_state = [tree1];
+    const walk_2_state = [tree2];
+
+    const walk_1_push = walk_1_state.push.bind(walk_1_state);
+    const walk_2_push = walk_2_state.push.bind(walk_2_state);
+
+    let node1;
+    let node2;
+
+    try {
+        while (walk_1_state.length && walk_2_state.length) {
+            node1 = walk_1_state.pop();
+            node2 = walk_2_state.pop();
+
+            if (!shallow_cmp(node1, node2)) return false;
+
+            node1._children_backwards(walk_1_push);
+            node2._children_backwards(walk_2_push);
+
+            if (walk_1_state.length !== walk_2_state.length) {
+                // Different number of children
+                return false;
+            }
+        }
+
+        return walk_1_state.length == 0 && walk_2_state.length == 0;
+    } catch (err) {
+        if (err instanceof assert.AssertionError) {
+            err.message += `\non line ${node2.start.line}`;
+            if (err.key) err.message += `\nKey "${err.key}" in ${node2.TYPE} node `;
+            else err.message += `\nIn ${node2.TYPE} node `;
+            err.message += '"' + node2.print_to_string() + '"';
+        }
+        throw err;
+    }
+
+    function shallow_cmp(a, b) {
+        for (const key of new Set([...Object.keys(a), ...Object.keys(b)])) {
+            if (key === "quote") {
+                // skip difference between empty string and undefined
+                if (a[key] == null && b[key] == "" || b[key] == null && a[key] == "") {
+                    continue
+                }
+            }
+            if (key === "is_generator" || key === "async") {
+                // skip difference between false and undefined
+                if (a[key] == null && b[key] == false || b[key] == null && a[key] == false) {
+                    continue
+                }
+            }
+            if (!ignore_keys.has(key)) {
+                try {
+                    shallow_cmp_key(key, a, b)
+                } catch (e) {
+                    if (e) e.key = key
+                    throw e
+                }
+            }
+        }
+        return true;
+    }
+
+    function shallow_cmp_key(key, a, b) {
+        const typeof_ = node =>
+            node === null ? 'null' :
+            node instanceof AST.AST_Node ? `AST_${node.TYPE}` :
+            typeof node;
+
+        if (!(key in a && key in b)) {
+            err(
+                key in a ? a[key] : '<key absent>',
+                key in b ? b[key] : '<key absent>',
+            );
+        }
+        if ((a[key] == null) !== (b[key] == null)) {
+            err(a[key], b[key]);
+        }
+        if (typeof_(a[key]) !== typeof_(b[key]) && a[key] != null) {
+            err(
+                a[key],
+                `<object of type ${typeof_(b[key])}>`,
+            );
+        }
+        if (a[key] == b[key]) {
+            return true; // sloppy equality is good for now
+        }
+        if (typeof a[key] === 'object' || typeof b[key] === 'object') {
+            if (a[key].constructor !== b[key].constructor) {
+                err(a[key], `<object>`);
+            }
+            if (a instanceof AST.AST_RegExp) {
+                if (a.source !== b.source || a.flags !== b.flags) {
+                    err(a[key], b[key]);
+                }
+                return true
+            }
+            if (Array.isArray(a[key])) {
+                if (a instanceof AST.AST_Node) {
+                    return true; // handled in recursion. This is something's body
+                }
+            }
+            if (a[key] instanceof AST.AST_Node) {
+                return true; // handled in recursion
+            }
+            throw new Error('Comparison not implemented')
+        }
+
+        err(a[key], b[key]);
+    }
+
+    function err(actual, expected) {
+        assert.strictEqual(actual, expected);
+        throw new Error("unreachable")
+    }
+}
+

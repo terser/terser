789d198300c0a0b966ea1798faa879158482731c
Huáng Jùnliàng <jlhwung@gmail.com>
2025-08-11 16:20:04 -0400

    support size

--- test/mocha/size.js
+++ test/mocha/size.js
@@ -9,4 +9,8 @@ describe("size", () => {
         assert.equal(parse("()=>{return 2}").size(), 5);
         assert.equal(parse("()=>2").size(), 5);
     });
+    it("approximates the size of variable declaration", () => {
+        assert.equal(parse("using x=null").size(), 12);
+        assert.equal(parse("async()=>{await using x=null}").size(), 30);
+    });
 });
